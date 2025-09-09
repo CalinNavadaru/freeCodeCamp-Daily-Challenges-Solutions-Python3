dfa345e0b14ca69018f3f487b95856192a8bc44c
CalinNavadaru <navadarucalin@yahoo.com>
2025-09-09 23:02:10 +0300

    Added solution for 2025-08-13

---
+++ 2025_08_13.py
@@ -0,0 +1,13 @@
+** start of main.py **
+
+def fibonacci_sequence(start_sequence, length):
+    if length == 0:
+        return []
+    if length <= 2:
+        return start_sequence[0: length]
+    for i in range(2, length):
+        start_sequence.append(start_sequence[i - 1] + start_sequence[i - 2])
+    return start_sequence
+
+** end of main.py **
+
