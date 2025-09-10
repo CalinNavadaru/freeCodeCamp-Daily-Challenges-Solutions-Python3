35df00380f3fb6ec0897a4641b384914d6d8a2ca
CalinNavadaru <navadarucalin@yahoo.com>
2025-09-10 09:45:36 +0300

    Added the solution for 2025-09-10

---
+++ 2025_09_10.py
@@ -0,0 +1,17 @@
+** start of main.py **
+
+def array_diff(arr1, arr2):
+    s1 = set(arr1)
+    s2 = set(arr2)
+    r = []
+    for e in arr2:
+        if e not in s1:
+            r.append(e)
+    for e in arr1:
+        if e not in s2:
+            r.append(e)
+    r.sort()
+    return r
+
+** end of main.py **
+
