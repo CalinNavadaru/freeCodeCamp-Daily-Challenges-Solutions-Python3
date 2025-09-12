c7f3e5d9430253f83d5533617f7adaa9bdc98d48
CalinNavadaru <navadarucalin@yahoo.com>
2025-09-12 08:09:37 +0300

    Remove unnecessary comments from 2025-08-13

--- 2025_08_13.py
+++ 2025_08_13.py
@@ -1,5 +1,3 @@
-** start of main.py **
-
 def fibonacci_sequence(start_sequence, length):
     if length == 0:
         return []
@@ -8,6 +6,3 @@ def fibonacci_sequence(start_sequence, length):
     for i in range(2, length):
         start_sequence.append(start_sequence[i - 1] + start_sequence[i - 2])
     return start_sequence
-
-** end of main.py **
-
