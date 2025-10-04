4e9aae2de50aacd2cdae99510e64fb9e214e260e
CalinNavadaru <navadarucalin@yahoo.com>
2025-10-04 16:02:07 +0300

    Adding the solution for 2025-10-04

---
+++ 2025_10/2025_10_04.py
@@ -0,0 +1,20 @@
+def classification(temp):
+    if temp >= 30_000:
+        return "O"
+
+    if temp >= 10_000:
+        return "B"
+
+    if temp >= 7_500:
+        return "A"
+
+    if temp >= 6_000:
+        return "F"
+
+    if temp >= 5_200:
+        return "G"
+
+    if temp >= 3_700:
+        return "K"
+
+    return "M"
